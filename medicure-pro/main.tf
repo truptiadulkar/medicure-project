1856d0cc7705c17b7b37690fdfaa1f482d012d5c
truptiadulkar <adulkar.trupti22@gmail.com>
2023-06-22 12:16:17 +0000

    files updated

--- medicure-pro/main.tf
+++ medicure-pro/main.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "test-server" {
+resource "aws_instance" "med-server" {
   ami                    = "ami-0f5ee92e2d63afc18"
   instance_type          = "t2.micro"
   key_name               = "keypairpem"
   vpc_security_group_ids = ["sg-0a5b8d6ca31ae2d81"]
   tags = {
-    Name = "test-server"
+    Name = "med-server"
   }
   
   provisioner "local-exec" {
@@ -19,11 +19,11 @@ resource "aws_instance" "test-server" {
   }
    
   provisioner "local-exec" {
-    command = "echo ${aws_instance.test-server.public_ip} > inventory"
+    command = "echo ${aws_instance.med-server.public_ip} > inventory"
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/finance-project/test-server/bankdeployplaybook.yml"
+    command = "ansible-playbook /var/lib/jenkins/workspace/medicure-project/med-server/bankdeployplaybook.yml"
   }
 }
 
