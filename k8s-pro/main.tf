abd3ec41b95c8a0735a1ec5a9828d96c1f5d4537
truptiadulkar <adulkar.trupti22@gmail.com>
2023-06-23 11:33:56 +0000

    changes done

--- k8s-pro/main.tf
+++ k8s-pro/main.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "medicure-pro" {
+resource "aws_instance" "k8s-pro" {
   ami                    = "ami-0f5ee92e2d63afc18"
-  instance_type          = "t2.micro"
+  instance_type          = "t2.medium"
   key_name               = "keypairpem"
   vpc_security_group_ids = ["sg-0a5b8d6ca31ae2d81"]
   tags = {
-    Name = "medicure-pro"
+    Name = "k8s-pro"
   }
   
   provisioner "local-exec" {
@@ -19,11 +19,11 @@ resource "aws_instance" "medicure-pro" {
   }
    
   provisioner "local-exec" {
-    command = "echo ${aws_instance.medicure-pro.public_ip} > inventory"
+    command = "echo ${aws_instance.k8s-pro.public_ip} > inventory"
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/medicure-peoject/medicure-pro/ansible-playbook.yml"
+    command = "ansible-playbook /var/lib/jenkins/workspace/k8s-pro/k8s-pro/ansible-playbook.yml"
   }
 }
 
